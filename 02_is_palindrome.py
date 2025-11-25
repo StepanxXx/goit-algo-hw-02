cf8b79bc4ce65868670fa507402347dceaa23b71
StepanxXx <stepanxxx@gmail.com>
2025-11-25 12:01:19 +0200

    test: add new test cases for `is_palindrome` function

--- 02_is_palindrome.py
+++ 02_is_palindrome.py
@@ -13,4 +13,5 @@ if __name__ == "__main__":
     print(is_palindrome("madam")) # True 
     print(is_palindrome("step on no pets")) # True
     print(is_palindrome(" Was  it a car rac  a tI saw")) # True
-    
+    print(is_palindrome("Was it a car or a cat I saw")) # True
+    print(is_palindrome("i")) # True
